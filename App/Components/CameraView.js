babb986ce280d2b7acd552c51934c8e364dd761b
Michelle Nguyen <ngynmt@gmail.com>
2016-07-25 16:52:53 -0700

    Revert "Revert "FIX/STYLE CHANGES, ASSETS FOLDER""

--- App/Components/CameraView.js
+++ App/Components/CameraView.js
@@ -94,7 +94,7 @@ class CameraView extends Component {
           aspect={Camera.constants.Aspect.fill}>
             <TouchableHighlight onPress={() => this._handleNextPage('CameraRollView')}>
               <View style={styles.goToCameraRoll}>
-                <Text style={styles.cameraRollText}>CAMERAROLL</Text>
+                <Image source={require('./Assets/cameraroll.png')}/>
               </View>
             </TouchableHighlight>
           <TouchableHighlight onPress={this.takePicture.bind(this)}>
